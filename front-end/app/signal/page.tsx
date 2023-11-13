dca167e9eb5fc243f5acdc277ea2cf9f04d42b8b
phc-quangg <quanggtangg@gmail.com>
2023-11-13 09:34:38 +0700

    try preact signal

--- front-end/app/signal/page.tsx
+++ front-end/app/signal/page.tsx
@@ -4,21 +4,25 @@ import { effect, signal } from "@preact/signals-react";
 import TodoList from "./components/list";
 import TodoForm from "./components/form";
 
-export const todos = signal(['wake up', 'drink water'])
+// export const todos = signal(['wake up', 'drink water'])
 
-effect(() => console.log(todos.value))
+// effect(() => console.log(todos.value))
 
 export default function Page() {
+    console.log('outer scope start')
+    setTimeout(() => console.log('timeout'), 0)
+    queueMicrotask(() => console.log('queue microtask'))
+    console.log('outer scope end')
 
     return (
         <div className="w-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-            <h3>Preact&apos;s Signal Demo</h3>
+            {/* <h3>Preact&apos;s Signal Demo</h3>
 
             <TodoForm todos={todos} />
 
             <div className="mt-10">
                 <TodoList todos={todos.value || []} />
-            </div>
+            </div> */}
         </div>
     )
 }
